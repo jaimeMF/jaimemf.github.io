55eb9ab1ff6c752bd65e0319e1853e1a87026af1
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2016-06-21 12:58:05 +0200

    Add analytics

--- website.py
+++ website.py
@@ -30,6 +30,7 @@ def key(key):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
+        app.config['ANAYLITICS'] = True
         freezer.freeze()
     else:
         app.run(debug=True)
